dba67cc77a96a483d67542ec16c2d70c3ee868aa
WaneT <wanetols@gmail.com>
2024-10-09 16:32:33 +0300

    Начнётся ад.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://wanetol72.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rdw-test-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'WaneTol72', // Usually your GitHub org/user name.
+  organizationName: 'wanetol72', // Usually your GitHub org/user name.
   projectName: 'rdw-test-docs', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
